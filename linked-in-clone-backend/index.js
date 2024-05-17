fedb4cea4d147d03f2b1c7315d4af4193bff6166
Priyanshu Govind Rao <98984204+Priyanshu020804@users.noreply.github.com>
2024-05-18 00:25:47 +0530

    added MongoDb Connection

--- linked-in-clone-backend/index.js
+++ linked-in-clone-backend/index.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const passport = require('passport');
 const { ExtractJwt , JwtStrategy } = require('passport-jwt');
+const mongoose = require('mongoose');
 
 const app=express();
+
+// to connect to MongoDb
+mongoose.connect( 'mongodb+srv://Priyanshu:best159@player@cluster0.nl4ozpm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0' ,
+    {
+        useNewUrlParser : true,
+        useUnifiedtopology : true,
+    }
+).then( (x) =>{
+    console.log("Connected to Mongo");
+}).catch( (err) =>{
+    console.log("error occured while connecting to Mongo");
+    console.log(err);
+});
 
 // passport jwt setup
 // jwt_payload : {identifier : userId}
--- linked-in-clone-backend/routes/auth.js
+++ linked-in-clone-backend/routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User =require('../models/User');
 const bcrypt = require('bcrypt');
+const {getToken} =require("../utils/helpers");
 
 router.post('/register' , async (req,res) => {
     // this function will handle the logic to register user
@@ -12,7 +13,7 @@ router.post('/register' , async (req,res) => {
         return res.status(400).json({err : "Invalid Request Body"});
     }
 
-    // check if a user with thta email exists or not
+    // check if a user with that email exists or not
     const existingUser = await User.findOne({email:email});
     if(existingUser){
         return res.status(402).json({err : "User With this Email Already Exists"});
@@ -24,5 +25,44 @@ router.post('/register' , async (req,res) => {
     const newUser = await User.create(newUserDetails);
 
     // Create a JWT token and return it to the user
+    const token = await getToken(email, newUser);
 
+    // We want to return following to the user
+    // 1. Actual user created
+    // 2. Token
+    const userToreturn = {...newUser.toJSON(), token};
+    delete userToreturn.password;
+    return res.status(200).json(userToreturn);
+});
+
+
+router.post('/login' , async (req,res) => {
+    // this function will handle the logic to login user
+
+    // get details from req.body
+    const {email, password} = req.body;
+    if(!email || !password){
+        return res.status(401).json({err : "Invalid userName or Password"});
+    }
+
+    // check if a user exist with that email exists or not
+    const user = await User.findOne({email:email});
+    if(!user){
+        return res.status(401).json({err : "Invalid userName or Password"});
+    }
+    // verify if password provided by user is correct or not
+    const isPasswordValid = await bcrypt.compare(password , user.password);
+    if(!isPasswordValid){
+        return res.status(401).json({err : "Invalid userName or Password"});
+    }
+
+    // Create a JWT token and return it to the user
+    const token = await getToken(email, user);
+
+    // We want to return following to the user
+    // 1. Actual user created
+    // 2. Token
+    const userToreturn = {...user.toJSON(), token};
+    delete userToreturn.password;
+    return res.status(200).json(userToreturn);
 });
